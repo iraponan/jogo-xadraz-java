73204b8ff01960777416c19af0beb5907cec6ce2
Iraponan Marinho <iraponan.aires@outlook.com>
2021-06-06 14:00:17 -0300

    Colocando peças no tabuleiro

--- src/aplicacao/UI.java
+++ src/aplicacao/UI.java
@@ -18,7 +18,7 @@ public class UI {
 		if (peca == null) {
 			System.out.print("-");
 		} else {
-			System.out.println(peca);
+			System.out.print(peca);
 		}
 		System.out.print(" ");
 	}
--- src/xadrez/PartidaDeXadrez.java
+++ src/xadrez/PartidaDeXadrez.java
@@ -1,12 +1,16 @@
 package xadrez;
 
+import tabuleiroDoJogo.Posicao;
 import tabuleiroDoJogo.Tabuleiro;
+import xadrez.pecas.Rei;
+import xadrez.pecas.Torre;
 
 public class PartidaDeXadrez {
 	private Tabuleiro tabuleiro;
 	
 	public PartidaDeXadrez() {
 		tabuleiro = new Tabuleiro(8, 8);
+		iniciarPartida();
 	}
 	
 	public PecaDeXadrez[][] getPecas() {
@@ -17,5 +21,11 @@ public class PartidaDeXadrez {
 			}
 		}
 		return mat;
+	}
+	
+	private void iniciarPartida() {
+		tabuleiro.posicaoDaPeca(new Torre(tabuleiro, Cor.BRANCO), new Posicao(2, 1));
+		tabuleiro.posicaoDaPeca(new Rei(tabuleiro, Cor.PRETO), new Posicao(0, 4));
+		tabuleiro.posicaoDaPeca(new Rei(tabuleiro, Cor.BRANCO), new Posicao(7, 4));
 	}
 }
